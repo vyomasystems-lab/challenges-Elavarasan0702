56f1922efc81eb0b4a89b15dc37d7cf9ed21d3d9
Elavarasan0702 <elavarasanp002@gmail.com>
2022-07-24 13:34:28 +0000

    update succesfully

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -23,4 +23,15 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
+
+    
+
+    IN=[0,1,0,1,1,0,1,1,0]
+    OUT=[0,0,0,0,1,0,0,1,0]
+    for i in range(len(IN)):
+        dut.inp_bit.value=IN[i]
+        await FallingEdge(dut.clk)
+        dut.log.info(f'INPUT={IN[i]:05} model={OUT[i]:05} OUT={int(dut.seq_seen .value):05}')
+        assert dut.seq_seen.value==OUT[i],"Incorrect operation {}!={}".format(dut.seq_seen.value,OUT[i])
+
     cocotb.log.info('#### CTB: Develop your test here! ######')
